c3c4a66495514d7c41de59243f1319379a71ac85
evachenyr <ychen840@gatech.edu>
2018-09-04 13:00:09 -0400

    Eva's first commit - I don't really know what I'm doing.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -8,30 +8,33 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
+	/**
+	 * Holds the persons real name
+	 */
 	private String name;
-	
+
 	/**
 	 * The constructor, takes in the persons
 	 * name
+	 *
 	 * @param pname the person's real name
 	 */
-	public Person3(String pname){
-	  name = pname;
+	public Person3(String pname) {
+		name = pname;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
+	 * @return the string representing the
+	 * object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
-	
+
 	/**
 	 * This method should take the string
 	 * input and return its reverse.
@@ -42,7 +45,13 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		//Person 3 put your implementation here
+		String reversed = new String("");
+		for (int i = input.length() - 1; i >= 0; i--) {
+			reversed = reversed + input.charAt(i);
+		}
+		return reversed;
 	}
 }
+
+
