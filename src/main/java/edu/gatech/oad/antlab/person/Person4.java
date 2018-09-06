04d7ed6e05b1f60c4f3ac3ca27cda85fefbe7309
Eva <ychen840@gatech.edu>
2018-09-06 08:37:06 -0400

    adding everything back in

---
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -0,0 +1,60 @@
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 4
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person4 {
+  /** Holds the persons real name */
+  private String name;
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+  public Person4(String pname) {
+    name = pname;
+  }
+    /**
+     * This method should return a string
+     * where each character is 1 greater 
+     * than its previous value.  So
+     * given "abc123" it should return
+     * "bcd234".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+      //Person 4 put your implementation here
+
+      String str = new String("");
+
+      for(int i = 0 ; i  < input.length(); i++) {
+
+        char prev = input.charAt(i);
+        int  iprev = (int) prev + 1;
+        char nextI = (char) iprev;
+        str = str + nextI;
+      }
+      return str;
+    }
+    
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the 
+     *         object
+     */
+    public String toString(String input) {
+      return name + calc(input);
+    }
+
+}
+
